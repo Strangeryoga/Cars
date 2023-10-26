3752e458af85a7c07b9392b9bc0a712974b0477b
Strangeryoga <tejasdoke4@gmail.com>
2023-10-26 23:26:56 +0530

    change images

--- script.js
+++ script.js
@@ -56,3 +56,11 @@ document.querySelector("#main").addEventListener("click", function(){
 
 
 
+Shery.imageEffect('#heroright #imagediv', {
+    style: 5,
+    config: {"a":{"value":1.96,"range":[0,30]},"b":{"value":-0.69,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.6665974437179937},"gooey":{"value":false},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.3,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.002,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+})
+
+
+
+
